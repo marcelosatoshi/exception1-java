b85b87682e67a39883642ccea545b02fda02ab91
Marcelo Gomes <marcelogomestsigti@gmail.com>
2022-02-17 02:52:56 -0300

    Bad solution

---
+++ src/application/Program.java
@@ -0,0 +1,20 @@
+package application;
+
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner cs = new Scanner(System.in);	
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		
+		
+	
+	
+	}
+
+}
