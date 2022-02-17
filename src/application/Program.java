c760d6b5763081be6d06601274495bc4c14a7106
Marcelo Gomes <marcelogomestsigti@gmail.com>
2022-02-17 03:08:02 -0300

    Bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,48 @@
 package application;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
+
+import model.entities.Reservation;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 
 		Scanner cs = new Scanner(System.in);	
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
+		System.out.print("Room number: ");
+		int room = cs.nextInt();
+		System.out.print("Check-in date (dd/MM/yyyy): ");
+		Date checkin = sdf.parse(cs.next());
+		System.out.print("Check-out date (dd/MM/yyyy): ");
+		Date checkout = sdf.parse(cs.next());
+		
+		Reservation reservation = new Reservation(room, checkin, checkout);
+		System.out.println("Reservation: " + reservation);
+		
+		System.out.println();
+		System.out.println("Enter data to update the reservation:");
+		System.out.print("Check-in date (dd/MM/yyyy): ");
+		checkin = sdf.parse(cs.next());
+		System.out.print("Check-out date (dd/MM/yyyy): ");
+		checkout = sdf.parse(cs.next());
 		
 		
-	
-	
+		
+		String error = reservation.updateDates(checkin, checkout);
+		if (error != null) {
+			System.out.println("Error in reservation" + error);
+			
+		} else {
+			System.out.println("Reservation: " + reservation);
+
+		}
+		
 	}
 
 }
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -13,7 +13,7 @@ public class Reservation {
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
 	
-	Reservation(Integer roomNumber, Date checkIn, Date chackOut) {
+	public Reservation(Integer roomNumber, Date checkIn, Date chackOut) {
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = chackOut;
@@ -40,9 +40,17 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 		}
 
-	public void upadteDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Reservation dates for update most be future dates";
+		} 
+		if (!checkOut.after(checkIn)) {
+			return "Check-out date must be after check-in date";
+		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 	}
 	
 	@Override
